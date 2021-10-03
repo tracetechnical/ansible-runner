efd3635d9bbba1ee4f2874aeacf1a7cd2f69a6e1
Alex Bush <alex.pheonixtechnical@gmail.com>
2021-10-03 12:28:13 +0100

    fix: build!

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM ubuntu:20.04
 
-RUN sudo apt update && sudo apt install -y ansible git
+RUN apt update && apt install -y ansible git
+
 COPY run.sh /opt/run.sh
+
 ENTRYPOINT ["/opt/run.sh"]
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo $VAULT_PASSWORD > /tmp/vaultpass
-sudo ansible-playbook --vault-password-file /tmp/vaultpass -i $INVENTORY_FILE $PLAYBOOK_FILE
+mkdir -p /tmp/repo
+cd /tmp/repo || exit
+git clone "$REPOSITORY" .
+echo "$VAULT_PASSWORD" > /tmp/vaultpass
+ansible-playbook --vault-password-file /tmp/vaultpass -i "$INVENTORY_FILE" "$PLAYBOOK_FILE"
