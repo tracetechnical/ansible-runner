9501b6bcfc52beb530fd440ff7272ac3d60a6455
Alex Bush <alex.pheonixtechnical@gmail.com>
2021-10-03 12:59:22 +0100

    fix: ssh error

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p /tmp/repo
 cd /tmp/repo || exit
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git clone "$REPOSITORY" .
 echo "$VAULT_PASSWORD" > /tmp/vaultpass
 ansible-playbook --vault-password-file /tmp/vaultpass -i "$INVENTORY_FILE" "$PLAYBOOK_FILE"
