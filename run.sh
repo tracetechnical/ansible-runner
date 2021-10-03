5daa05f226f5e66ec136a5f545efdc98e81c4110
Alex Bush <alex.pheonixtechnical@gmail.com>
2021-10-03 13:30:37 +0100

    feat: add cd to ansible directory

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ mkdir -p /tmp/repo
 cd /tmp/repo || exit
 git clone "$REPOSITORY" .
 echo "$VAULT_PASSWORD" > /tmp/vaultpass
+cd "$ANSIBLE_DIRECTORY" || exit
 ansible-playbook --vault-password-file /tmp/vaultpass -i "$INVENTORY_FILE" "$PLAYBOOK_FILE"
